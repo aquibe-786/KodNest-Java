3fd9ae992437760e8a3b8f4d8d9ad265d591ea90
Md Aquibe Jawed <aquibejawed786.aj@gmail.com>
2026-09-03 11:54:39 +0530

    This is my first Update

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,6 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
-       //Write your code here
+       System.out.println("Welcome to Java!");
+       System.out.println("I'm in java track!");
     }
 }
