b9b5a4bf4a0070384753167e7e5074209283995c
Md Aquibe Jawed <aquibejawed786.aj@gmail.com>
2026-09-05 00:05:39 +0530

    Practice

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,6 +1,9 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
-       System.out.println("Welcome to Java!");
-       System.out.println("I'm in java track!");
+        // Display the student's learning track.
+        System.out.println("Track: Java Development");
+
+        // Display the student's daily commitment.
+        System.out.println("Daily Practice: 8 Hours");
     }
 }
